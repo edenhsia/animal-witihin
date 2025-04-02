d9895f2864de96f8a6a916ca17e21b04012c33fb
Eden <edenhsia@gmail.com>
2025-04-02 18:02:09 +0800

    feat: add React Router with basic route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeToggle } from './components/ThemeToggle'
 import ThemeProvider from './providers/ThemeProvider'
 import QuizQuestion from './components/QuizQuestion'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
     <ThemeProvider>
       <ThemeToggle />
+
       <main className="min-h-screen flex items-center justify-center">
-        <QuizQuestion />
+        <Routes>
+          <Route path="/quiz" element={<QuizQuestion />} />
+        </Routes>
       </main>
     </ThemeProvider>
   )
